b23ced922f9e43319255b72f2b9be1ec02f484a8
Aman Verma <amanve.bsp07@gmail.com>
2025-05-17 21:02:07 +0530

    minor changes

--- microservices/backend/src/main/java/com/hospital/config/SecurityConfig.java
+++ microservices/backend/src/main/java/com/hospital/config/SecurityConfig.java
@@ -90,6 +90,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Frontend URL
+        configuration.setAllowedOrigins(List.of("http://192.168.49.2:32697"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
