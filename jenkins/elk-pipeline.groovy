4cda81c474a82039100ab34f6a023c08cc93b918
Aman Verma <amanve.bsp07@gmail.com>
2025-05-18 23:17:31 +0530

    updating elkstack

--- jenkins/elk-pipeline.groovy
+++ jenkins/elk-pipeline.groovy
@@ -8,13 +8,22 @@ pipeline {
     stages {
         stage('Deploy ELK Stack') {
             steps {
+
+                echo "Applying Logstash..."
+                sh 'kubectl apply -f elk/logstash/logstash-configmap.yaml -n demo-basic'
+                sh 'kubectl apply -f elk/logstash/logstash-deployment.yaml -n demo-basic'
+                
                 echo "Applying Elasticsearch..."
                 sh 'kubectl apply -f elk/elasticsearch/elasticsearch-deployment.yaml -n demo-basic'
 
                 echo "Applying Kibana..."
                 sh 'kubectl apply -f elk/kibana/kibana-deployment.yaml -n demo-basic'
 
-                echo "Applying Filebeat..."
+                echo "Setting up Filebeat RBAC..."
+                sh 'kubectl apply -f elk/filebeat/filebeat-rbac.yaml -n demo-basic'
+
+                echo "Applying Filebeat Config and DaemonSet..."
+                sh 'kubectl apply -f elk/filebeat/filebeat-configmap.yaml -n demo-basic'
                 sh 'kubectl apply -f elk/filebeat/filebeat-k8s.yaml -n demo-basic'
             }
         }
