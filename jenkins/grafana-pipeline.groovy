c2d9016ba8a9addd64e7344558643330aa88e60b
Aman Verma <amanve.bsp07@gmail.com>
2025-05-18 12:18:22 +0530

    minor changes in grafana groovy file

--- jenkins/grafana-pipeline.groovy
+++ jenkins/grafana-pipeline.groovy
@@ -5,10 +5,19 @@ pipeline {
         stage('Deploy Grafana') {
             steps {
                 script {
-                    sh 'kubectl apply -f monitoring/grafana/deployment.yaml -n monitoring'
-                    sh 'kubectl apply -f monitoring/grafana/service.yaml -n monitoring'
-                    sh 'kubectl create configmap grafana-datasource --from-file=monitoring/grafana/datasources.yaml -n monitoring --dry-run=client -o yaml | kubectl apply -f -'
-                    sh 'kubectl create configmap grafana-dashboard --from-file=monitoring/grafana/dashboard-config.yaml -n monitoring --dry-run=client -o yaml | kubectl apply -f -'
+                    sh '''
+                    # Delete old deployment if it exists
+                    kubectl delete deployment grafana -n monitoring --ignore-not-found
+                    kubectl delete svc grafana-service -n monitoring --ignore-not-found
+                    kubectl delete configmap grafana-datasource -n monitoring --ignore-not-found
+                    kubectl delete configmap grafana-dashboard -n monitoring --ignore-not-found
+
+                    # Apply updated files
+                    kubectl create configmap grafana-datasource --from-file=monitoring/grafana/datasources.yaml -n monitoring --dry-run=client -o yaml | kubectl apply -f -
+                    kubectl create configmap grafana-dashboard --from-file=monitoring/grafana/dashboard-config.yaml -n monitoring --dry-run=client -o yaml | kubectl apply -f -
+                    kubectl apply -f monitoring/grafana/deployment.yaml -n monitoring
+                    kubectl apply -f monitoring/grafana/service.yaml -n monitoring
+                    '''
                 }
             }
         }
